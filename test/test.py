7e2e46e27dc04ad49e05ca81f8ba5cacb7c2b63f
qf <qf0129@qq.com>
2017-05-02 20:37:24 +0800

    temp

--- test/test.py
+++ test/test.py
@@ -3,7 +3,6 @@ from bottle import route,get,run,static_file
 
 @route('/')
 def index():
-    return static_file('index.html', root='C:/Users/qf/Desktop')
+    return static_file('test.html', root='/home/gll/work/qfdate/test')
 
-
-run(host='0.0.0.0',port=80)
+run(host='0.0.0.0',port=8080)
